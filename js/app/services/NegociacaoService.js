b39cba8f30a0a7330f4e10bce90e9299c3a4b1d2
Lucas Adams <adams.lucass@gmail.com>
2017-05-09 00:09:53 -0300

    commit modulo 4 com indexedDB

--- js/app/services/NegociacaoService.js
+++ js/app/services/NegociacaoService.js
@@ -1,14 +1,14 @@
 class NegociacaoService {
-    
+
     constructor() {
-        
+
         this._http = new HttpService();
     }
-    
+
     obterNegociacoesDaSemana() {
-       
-       return new Promise((resolve, reject) => {
-        
+
+        return new Promise((resolve, reject) => {
+
             this._http
                 .get('negociacoes/semana')
                 .then(negociacoes => {
@@ -18,14 +18,14 @@ class NegociacaoService {
                 .catch(erro => {
                     console.log(erro);
                     reject('Não foi possível obter as negociações da semana');
-                });  
-       });        
+                });
+        });
     }
-    
+
     obterNegociacoesDaSemanaAnterior() {
-       
-       return new Promise((resolve, reject) => {
-        
+
+        return new Promise((resolve, reject) => {
+
             this._http
                 .get('negociacoes/anterior')
                 .then(negociacoes => {
@@ -35,16 +35,16 @@ class NegociacaoService {
                 .catch(erro => {
                     console.log(erro);
                     reject('Não foi possível obter as negociações da semana anterior');
-                });  
-       }); 
-       
-        
+                });
+        });
+
+
     }
-    
+
     obterNegociacoesDaSemanaRetrasada() {
-       
-       return new Promise((resolve, reject) => {
-        
+
+        return new Promise((resolve, reject) => {
+
             this._http
                 .get('negociacoes/retrasada')
                 .then(negociacoes => {
@@ -54,11 +54,11 @@ class NegociacaoService {
                 .catch(erro => {
                     console.log(erro);
                     reject('Não foi possível obter as negociações da semana retrasada');
-                });  
-       }); 
-    }    
-    
-    
+                });
+        });
+    }
+
+
     obterNegociacoes() {
 
         return new Promise((resolve, reject) => {
@@ -77,6 +77,53 @@ class NegociacaoService {
 
             }).catch(erro => reject(erro));
         });
-    }    
-}
+    }
 
+    cadastra(negociacao) {
+        return ConnectionFactory
+            .getConnection()
+            .then(conn => new NegociacaoDao(conn))
+            .then(dao => dao.adiciona(negociacao))
+            .then(() => 'Negociacao adicionada com sucesso!')
+            .catch(erro => {
+                console.log(erro);
+                throw new Error('Não foi possivel adicionar a negociacao')
+            });
+    }
+
+    lista() {
+        return ConnectionFactory
+            .getConnection()
+            .then(conn => new NegociacaoDao(conn))
+            .then(dao => dao.listaTodos())
+            .catch(erro => {
+                console.log(erro);
+                throw new Error('Não foi possivel obter as negociações!');
+            });
+    }
+
+    apaga() {
+        return ConnectionFactory
+            .getConnection()
+            .then(conn => new NegociacaoDao(conn))
+            .then(dao => dao.listaTodos())
+            .then(() => 'Negociacoes apagadas com sucesso!')
+            .catch(erro => {
+                console.log(erro);
+                throw new Error('Não foi possivel apagar as negociacao')
+            });
+    }
+
+    importa(listaAtual) {
+        return this.obterNegociacoes()
+            .then(negociacoes =>
+                negociacoes.filter(neg =>
+                    !listaAtual.some(negExistente => JSON.stringify(neg) === JSON.stringify(negExistente))
+                )
+            )
+            .catch(erro => {
+                console.log(erro);
+                throw new Erro('Não foi possivel buscar negociacoes para importar');
+            });
+    }
+}
